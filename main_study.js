c6b83120ebff0bccc307cb6683ff13977b58e5a7
kimsh1017 <kimsh8580@gmail.com>
2022-04-10 23:44:53 +0900

    change_js_study

--- main_study.js
+++ main_study.js
@@ -4,15 +4,8 @@ const moreBtn = document.querySelector('.nav_bar .toggle_button');
 const menu_list = document.querySelector('.menu_list');
 const menu = document.querySelector('.nav_bar');
 
-const study1 = document.querySelector('.study .desktop .study_1');
-const study2 = document.querySelector('.study .desktop .study_2');
-const study3 = document.querySelector('.study .desktop .study_3');
-const study4 = document.querySelector('.study .desktop .study_4');
-
-const locations = [ document.getElementById('card1').offsetTop, 
-document.getElementById('card2').offsetTop,
-document.getElementById('card3').offsetTop,
-document.getElementById('card4').offsetTop ];
+const study = document.querySelectorAll('.study .desktop li');
+const locations = document.querySelectorAll('.study .cards .card');
 
 moreBtn.addEventListener('click', () => {
     moreBtn.classList.toggle('clicked');
@@ -20,19 +13,19 @@ moreBtn.addEventListener('click', () => {
     menu_list.classList.toggle('clamp');
 });
 
-study1.addEventListener('click', ()=>{
-    window.scrollTo({top:locations[0] - 100, left:0 ,behavior:'smooth'})
+
+study[0].addEventListener('click', ()=>{
+    window.scrollTo({top:locations[0].offsetTop - 100, left:0 ,behavior:'smooth'});
 });
 
-
-study2.addEventListener('click', ()=>{
-    window.scrollTo({top:locations[1] - 100, left:0 ,behavior:'smooth'});
+study[1].addEventListener('click', ()=>{
+    window.scrollTo({top:locations[1].offsetTop - 100, left:0 ,behavior:'smooth'});
 });
 
-study3.addEventListener('click', ()=>{
-    window.scrollTo({top:locations[2] - 100, left:0 ,behavior:'smooth'});
+study[2].addEventListener('click', ()=>{
+    window.scrollTo({top:locations[2].offsetTop - 100, left:0 ,behavior:'smooth'});
 });
 
-study4.addEventListener('click', ()=>{
-    window.scrollTo({top:locations[3] - 100, left:0 ,behavior:'smooth'});
+study[3].addEventListener('click', ()=>{
+    window.scrollTo({top:locations[3].offsetTop - 100, left:0 ,behavior:'smooth'});
 });
